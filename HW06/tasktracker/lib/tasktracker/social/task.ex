7f217d7ca7a0b5dca3669325414ecbb159b73e1b
Tejas Parab <tejas.bhaskar.parab@gmail.com>
2018-02-18 20:22:31 -0500

    hw06 update

--- HW06/tasktracker/lib/tasktracker/social/task.ex
+++ HW06/tasktracker/lib/tasktracker/social/task.ex
@@ -8,6 +8,7 @@ defmodule Tasktracker.Social.Task do
     field(:completed, :boolean, default: false)
     field(:title, :string)
     belongs_to(:user, Tasktracker.Accounts.User, foreign_key: :assigned_id)
+    field(:time_taken, :integer)
 
     timestamps()
   end
@@ -15,7 +16,7 @@ defmodule Tasktracker.Social.Task do
   @doc false
   def changeset(%Task{} = task, attrs) do
     task
-    |> cast(attrs, [:title, :body, :completed, :assigned_id])
-    |> validate_required([:title, :body, :completed, :assigned_id])
+    |> cast(attrs, [:title, :body, :completed, :assigned_id, :time_taken])
+    |> validate_required([:title, :body, :completed, :assigned_id, :time_taken])
   end
 end
